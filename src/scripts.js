47fa59b04df8d7830bb328eba21708121364b060
Zahra947 <zhr.rabiei@gmail.com>
2021-07-02 19:44:55 +0430

    provided all related coding files of app

---
+++ src/scripts.js
@@ -0,0 +1,101 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day} 
+  <br/>  ${hours}:${minutes}`;
+}
+
+function showTemp(response) {
+  //console.response.data;
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+function search(city) {
+  let apiKey = "9e426d3dc7d76c6df8a50964d68d9730";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+
+function handleSubmit(event) {
+  //debugger;
+  event.preventDefault();
+  let city = document.querySelector("#cityInput").value;
+  search(city);
+  //let cityElement = document.querySelector("#city");
+  //let cityInput = document.querySelector("#cityInput");
+  //console.log(cityInput);
+  //cityElement.innerHTML = cityInput.value;
+}
+
+function searchLocation(position) {
+  debugger;
+  let apiKey = "9e426d3dc7d76c6df8a50964d68d9730";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(showTemp);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 104;
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 40;
+}
+//feature 1
+let day = document.querySelector("#currentDay");
+console.log(day);
+let current = new Date();
+day.innerHTML = formatDate(current);
+//feature 2
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+//feature 3
+//let fahrenheitLink = document.querySelector("#fahrenheit-link");
+//fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+//let celsiusLink = document.querySelector("#celsius-link");
+//celsiusLink.addEventListener("click", convertToCelsius);
+
+//real weather
+let currentLocationButton = document.querySelector("#current-location");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+search("Tehran");
